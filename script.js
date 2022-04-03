23f36e6d49e2c50d77202dbe8f8e9529a0406953
Anita Chengalva <anita.chengalva@gmail.com>
2022-04-02 23:28:22 -0700

    fetch api workinggit add .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID=8c8e55781e1b60ae6ad3fafe04b05cc8", function(data){
-    console.log(data);
-});
+// fetch API
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID=8c8e55781e1b60ae6ad3fafe04b05cc8')
+  .then(response => response.json())
+  .then(data => console.log(data));
