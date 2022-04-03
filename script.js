81a9941902d5ea0000c9657ac1f2a7ce739e4382
Anita Chengalva <anita.chengalva@gmail.com>
2022-04-02 23:56:25 -0700

    submit

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID=8c8e55781e1b60ae6ad3fafe04b05cc8')
   .then(response => response.json())
   .then(data => console.log(data));
+  
+function weather(data) {
+    console.log(data);
+
+    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = data.main.temp;
+    var weather = data.weather[0].main;
+
+    // icon displays current weather conditions 
+    $(".icon").attr('src', icon);
+    $(".weather").append(weather);
+    $(".temp").append(temp);
+}
